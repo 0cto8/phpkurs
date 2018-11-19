bf5da2be9b1c1acc1b464af8b3fcce983c081a70
0cto8 <38282261+0cto8@users.noreply.github.com>
2018-11-19 10:48:37 +0100

    Add files via upload

---
+++ seitevier2.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>php-Script 2</title>
+	</head>
+	<body>
+		<div></div>
+<?php
+echo "Was geht Welt, alles fit bei dir";
+phpinfo();
+?>
+</body>
+</html>
